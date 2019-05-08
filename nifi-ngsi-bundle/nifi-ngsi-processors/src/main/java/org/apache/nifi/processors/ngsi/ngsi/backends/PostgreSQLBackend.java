6946c17a2d92af0f06f98240d864b163b1c9cb4a
anmunoz <andres.max36@gmail.com>
2019-05-08 12:53:47 +0200

    Template for adding column support to Postgresql

--- nifi-ngsi-bundle/nifi-ngsi-processors/src/main/java/org/apache/nifi/processors/ngsi/ngsi/backends/PostgreSQLBackend.java
+++ nifi-ngsi-bundle/nifi-ngsi-processors/src/main/java/org/apache/nifi/processors/ngsi/ngsi/backends/PostgreSQLBackend.java
@@ -8,40 +8,56 @@ import java.util.Iterator;
 
 public class PostgreSQLBackend {
 
-    public ArrayList listOfFields (String attrPersistence){
+    public ArrayList listOfFields (String attrPersistence, Entity entity){
+
         ArrayList<String> aggregation = new ArrayList<>();
-        if (attrPersistence.compareToIgnoreCase("row")==0){
-            aggregation.add(NGSIConstants.RECV_TIME_TS);
-            aggregation.add(NGSIConstants.RECV_TIME);
-            aggregation.add(NGSIConstants.FIWARE_SERVICE_PATH);
-            aggregation.add(NGSIConstants.ENTITY_ID);
-            aggregation.add(NGSIConstants.ENTITY_TYPE);
+        aggregation.add(NGSIConstants.RECV_TIME_TS);
+        aggregation.add(NGSIConstants.RECV_TIME);
+        aggregation.add(NGSIConstants.FIWARE_SERVICE_PATH);
+        aggregation.add(NGSIConstants.ENTITY_ID);
+        aggregation.add(NGSIConstants.ENTITY_TYPE);
+
+        if ("row".equalsIgnoreCase(attrPersistence)){
             aggregation.add(NGSIConstants.ATTR_NAME);
             aggregation.add(NGSIConstants.ATTR_TYPE);
             aggregation.add(NGSIConstants.ATTR_VALUE);
             aggregation.add(NGSIConstants.ATTR_MD);
-        }else if(attrPersistence.compareToIgnoreCase("column")==0){
-            //TBD
+        }
+        else if("column".equalsIgnoreCase(attrPersistence)){
+
+            ArrayList<Attributes> attributes = entity.getEntityAttrs();
+            if (attributes != null && !attributes.isEmpty()) {
+                for (Attributes attribute : attributes) {
+                    String attrName = attribute.getAttrName();
+                    aggregation.add(attrName);
+                    aggregation.add(attrName + "_md");
+                } // for
+
+            } // if
+
             System.out.println("column");
         }
         return aggregation;
     }
 
-    public String getValuesForInsert(Entity entity, long creationTime, String fiwareServicePath) {
+    public String getValuesForInsert(String attrPersistence,Entity entity, long creationTime, String fiwareServicePath) {
         String valuesForInsert = "";
-            for (int i = 0; i < entity.getEntityAttrs().size(); i++) {
-                if (i == 0) {
-                    valuesForInsert += "(";
 
-                } else {
-                    valuesForInsert += ",(";
-                } // if else
+        for (int i = 0; i < entity.getEntityAttrs().size(); i++) {
+            if (i == 0) {
+                valuesForInsert += "(";
 
-                valuesForInsert += "'" + creationTime + "'";
-                valuesForInsert += ",'" + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(creationTime) + "'";
-                valuesForInsert += ",'" + fiwareServicePath.replace("/", "") + "'";
-                valuesForInsert += ",'" + entity.getEntityId() + "'";
-                valuesForInsert += ",'" + entity.getEntityType() + "'";
+            } else {
+                valuesForInsert += ",(";
+            } // if else
+
+            valuesForInsert += "'" + creationTime + "'";
+            valuesForInsert += ",'" + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(creationTime) + "'";
+            valuesForInsert += ",'" + fiwareServicePath.replace("/", "") + "'";
+            valuesForInsert += ",'" + entity.getEntityId() + "'";
+            valuesForInsert += ",'" + entity.getEntityType() + "'";
+
+            if ("row".equalsIgnoreCase(attrPersistence)){
                 valuesForInsert += ",'" + entity.getEntityAttrs().get(i).getAttrName() + "'";
                 valuesForInsert += ",'" + entity.getEntityAttrs().get(i).getAttrType()  + "'";
                 valuesForInsert += ",'" + entity.getEntityAttrs().get(i).getAttrValue() + "'";
@@ -51,14 +67,26 @@ public class PostgreSQLBackend {
                     valuesForInsert += ",'[]'";
                 }
                 valuesForInsert += ")";
+
+            } else if("column".equalsIgnoreCase(attrPersistence)){
+                for (Attributes attribute : entity.getEntityAttrs()) {
+                    valuesForInsert += ",'" + entity.getEntityAttrs().get(i).getAttrValue() + "'";
+                    if (entity.getEntityAttrs().get(i).getAttrMetadata() != null) {
+                        valuesForInsert += ",'" + entity.getEntityAttrs().get(i).getMetadataString() + "'";
+                    } else {
+                        valuesForInsert += ",'[]'";
+                    }
+                }
+                valuesForInsert += ")";
             } // for
 
+        }
         return valuesForInsert;
     } // getValuesForInsert
 
 
-    public String getFieldsForCreate(String attrPersistence) {
-        Iterator it = listOfFields(attrPersistence).iterator();
+    public String getFieldsForCreate(String attrPersistence, Entity entity) {
+        Iterator it = listOfFields(attrPersistence,entity).iterator();
         String fieldsForCreate = "(";
         boolean first = true;
         while (it.hasNext()) {
@@ -73,11 +101,11 @@ public class PostgreSQLBackend {
         return fieldsForCreate + ")";
     } // getFieldsForCreate
 
-    public String getFieldsForInsert(String attrPersistence) {
+    public String getFieldsForInsert(String attrPersistence,Entity entity) {
 
         String fieldsForInsert = "(";
         boolean first = true;
-        Iterator it = listOfFields(attrPersistence).iterator();
+        Iterator it = listOfFields(attrPersistence, entity).iterator();
         while (it.hasNext()) {
             if (first) {
                 fieldsForInsert += (String) it.next();
@@ -110,9 +138,9 @@ public class PostgreSQLBackend {
         return query;
     }
 
-    public String createTable(String schemaName,String tableName, String attrPersistence){
+    public String createTable(String schemaName,String tableName, String attrPersistence, Entity entity){
 
-        String query= "create table if not exists "+schemaName+"." + tableName + " " + getFieldsForCreate(attrPersistence) + ";";
+        String query= "create table if not exists "+schemaName+"." + tableName + " " + getFieldsForCreate(attrPersistence,entity) + ";";
         return query;
     }
 
@@ -122,54 +150,54 @@ public class PostgreSQLBackend {
         String entityId = (enableLowercase) ? entity.getEntityId().toLowerCase() : entity.getEntityId();
         String entityType = (enableLowercase) ? entity.getEntityType().toLowerCase() : entity.getEntityType();
 
-            if (enableEncoding) {
-                switch (dataModel) {
-                    case "db-by-service-path":
-                        tableName = NGSICharsets.encodePostgreSQL(servicePath);
-                        break;
-                    case "db-by-entity":
-                        tableName = NGSICharsets.encodePostgreSQL(servicePath)
-                                + CommonConstants.CONCATENATOR
-                                + NGSICharsets.encodePostgreSQL(entityId)
-                                + CommonConstants.CONCATENATOR
-                                + NGSICharsets.encodePostgreSQL(entityType);
-                        break;
-                    default:
-                        System.out.println("Unknown data model '" + dataModel
-                                + "'. Please, use dm-by-service-path, dm-by-entity or dm-by-attribute");
-                } // switch
-            } else {
-                switch (dataModel) {
-                    case "db-by-service-path":
-                        if ("/".equals(servicePath)) {
-                            System.out.println("Default service path '/' cannot be used with "
-                                    + "dm-by-service-path data model");
-                        } // if
+        if (enableEncoding) {
+            switch (dataModel) {
+                case "db-by-service-path":
+                    tableName = NGSICharsets.encodePostgreSQL(servicePath);
+                    break;
+                case "db-by-entity":
+                    tableName = NGSICharsets.encodePostgreSQL(servicePath)
+                            + CommonConstants.CONCATENATOR
+                            + NGSICharsets.encodePostgreSQL(entityId)
+                            + CommonConstants.CONCATENATOR
+                            + NGSICharsets.encodePostgreSQL(entityType);
+                    break;
+                default:
+                    System.out.println("Unknown data model '" + dataModel
+                            + "'. Please, use dm-by-service-path, dm-by-entity or dm-by-attribute");
+            } // switch
+        } else {
+            switch (dataModel) {
+                case "db-by-service-path":
+                    if ("/".equals(servicePath)) {
+                        System.out.println("Default service path '/' cannot be used with "
+                                + "dm-by-service-path data model");
+                    } // if
 
-                        tableName = NGSICharsets.encode(servicePath, true, false);
-                        break;
-                    case "db-by-entity":
-                        String truncatedServicePath = NGSICharsets.encode(servicePath, true, false);
-                        tableName = (truncatedServicePath.isEmpty() ? "" : truncatedServicePath + '_')
-                                + NGSICharsets.encode(entityId, false, true) + "_"
-                                + NGSICharsets.encode(entityType, false, true);
-                        break;
-                    default:
-                        System.out.println("Unknown data model '" + dataModel
-                                + "'. Please, use DMBYSERVICEPATH, DMBYENTITY or DMBYATTRIBUTE");
-                        break;
-                } // switch
-            } // if else
+                    tableName = NGSICharsets.encode(servicePath, true, false);
+                    break;
+                case "db-by-entity":
+                    String truncatedServicePath = NGSICharsets.encode(servicePath, true, false);
+                    tableName = (truncatedServicePath.isEmpty() ? "" : truncatedServicePath + '_')
+                            + NGSICharsets.encode(entityId, false, true) + "_"
+                            + NGSICharsets.encode(entityType, false, true);
+                    break;
+                default:
+                    System.out.println("Unknown data model '" + dataModel
+                            + "'. Please, use DMBYSERVICEPATH, DMBYENTITY or DMBYATTRIBUTE");
+                    break;
+            } // switch
+        } // if else
 
-            if (tableName.length() > NGSIConstants.POSTGRESQL_MAX_NAME_LEN) {
-                throw new SQLException("Building table name '" + tableName
-                        + "' and its length is greater than " + NGSIConstants.POSTGRESQL_MAX_NAME_LEN);
-            } // if
+        if (tableName.length() > NGSIConstants.POSTGRESQL_MAX_NAME_LEN) {
+            throw new SQLException("Building table name '" + tableName
+                    + "' and its length is greater than " + NGSIConstants.POSTGRESQL_MAX_NAME_LEN);
+        } // if
         return tableName;
     }
 
     public String insertQuery (Entity entity, long creationTime, String fiwareServicePath, String schemaName, String tableName, String dataModel){
-        String query="Insert into "+schemaName+"."+ tableName + " " +this.getFieldsForInsert(dataModel)+ " values " +this.getValuesForInsert(entity, creationTime, fiwareServicePath);
+        String query="Insert into "+schemaName+"."+ tableName + " " +this.getFieldsForInsert(dataModel,entity)+ " values " +this.getValuesForInsert(dataModel,entity, creationTime, fiwareServicePath);
         return query;
     }
 }
